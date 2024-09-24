4ee54aa88e7c147edee0b0be008f7b642a62d4a8
babingthon <babingthon.bandeira@gmail.com>
2024-09-24 19:53:30 -0300

    configuracao do banco

--- minimal-api/Infraestrutura/DB/DbContexto.cs
+++ minimal-api/Infraestrutura/DB/DbContexto.cs
@@ -1,7 +1,33 @@
+using System.Security;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using minimal_api.Dominio.Entidades;
+
 namespace minimal_api.Infraestrutura.DB
 {
-    public class DbContexto
+    public class DbContexto : DbContext
     {
-        
+        private readonly IConfiguration _configuracaoAppSetting;
+        public DbContexto(IConfiguration configuracaoAppSetting)
+        {
+            _configuracaoAppSetting = configuracaoAppSetting;
+        }
+        public DbSet<Administrador> Administradores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var stringConexao = _configuracaoAppSetting.GetConnectionString("DefaultConnection")?.ToString();
+
+                if (!string.IsNullOrEmpty(stringConexao))
+                {
+                    optionsBuilder.UseSqlServer(stringConexao);
+                }
+            }
+
+        }
+
+
     }
 }
--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using minimal_api.Infraestrutura.DB;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
